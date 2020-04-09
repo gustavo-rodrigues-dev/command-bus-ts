38653f3ad3068e2d6dd782789d230edf6e483012
Gustavo S. Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2020-04-09 08:36:08 -0300

    create decorator of commandHandle

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import ContextManager from './context/context-manager';
+import { CommandContext } from './context';
+
+export function handleCommand(
+  namespace: string,
+  methodCall: string = 'execute',
+) {
+  return function (target: any) {
+    const instance = ContextManager.getInstance();
+    const call = target.prototype[methodCall];
+    if (!call) {
+      throw new Error('Method Call of handle dont exists');
+    }
+    const command: CommandContext = {
+      namespace,
+      handle: call,
+    };
+
+    instance.set(command);
+  };
+}
