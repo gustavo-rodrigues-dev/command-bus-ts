bc4431a9213eafb574eeac040c1b191de5b7c9b1
Gustavo S. Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2020-04-25 17:40:32 -0300

    register base tests of solution

---
+++ test/unit/domain/command-bus/service/command-bus.spec.ts
@@ -0,0 +1,56 @@
+import { before } from 'mocha';
+import { expect } from 'chai';
+import { Dispatch }  from '../../../../../src/domain/command-bus/';
+import { CommandBus } from '../../../../../src/domain/command-bus/service/command-bus';
+
+describe('CommandBus', () => {
+  let context: Dispatch;
+  beforeEach(() => {
+    context = new CommandBus();
+  });
+  describe('getCommand()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+  describe('updateContext()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+  describe('registerCommand()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+  describe('registerCommand()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+  describe('use()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+  describe('subscribeCommand()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+
+    it('Should subscribe a command successfully');
+    it('Should not be two identical subscriptions to a command');
+  });
+  describe('unsubscribeCommand()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+    it('Should unsubscribe a command successfully');
+    it('Should return error when removing a non-existent subscription on a command');
+  });
+  describe('execute()', () => {
+    before(() => {
+      context = new CommandBus();
+    });
+  });
+});
---
+++ test/unit/domain/middleware/service/middleware-storm.spec.ts
@@ -0,0 +1,11 @@
+
+import { before } from 'mocha';
+import { expect } from 'chai';
+
+describe('MiddlewareStorm', () => {
+  describe('invoke()', () => {
+    it('Should invoke middleware successfully');
+    it('Should invoke alist of middleware successfully');
+    it('Should return error if any middleware fails');
+  });
+});
